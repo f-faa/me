f4d5975da30e3a21354243e749975c4a90a7d996
faa <76886727+faa-faa@users.noreply.github.com>
2024-02-16 19:59:51 +0600

    removed initial loading screen from index.js

--- index.js
+++ index.js
@@ -4,30 +4,22 @@ $(document).ready(function(){
       event.preventDefault();
       var hash = this.hash;
       $('body,html').animate({
-      scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top
       }, 1200, function(){
-      window.location.hash = hash;
-     });
-     } 
-    });
+        window.location.hash = hash;
+      });
+    } 
+  });
 });
 
 var width = $(window).width(); 
 
 window.onscroll = function(){
-if ((width >= 900)){
+  if ((width >= 900)){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#middle").css("background-size","150% auto");
+      $("#middle").css("background-size","150% auto");
     }else{
-        $("#middle").css("background-size","100% auto");        
+      $("#middle").css("background-size","100% auto");        
     }
-}
+  }
 };
-
-setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-    },800);
-},1450);

1546911acaaf69d04b5bff5ed3cdd87446ab02bf
faa <76886727+f-faa@users.noreply.github.com>
2024-03-02 10:25:43 +0600

    Add files via upload

---
+++ osu!/osu!.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Call the function with the element you want to disable selection for
+    disableSelection(document.querySelector('h1'));
+});
+
+function disableSelection(element) {
+    // For most browsers
+    element.setAttribute('unselectable', 'on');
+    // For IE9 and below
+    element.setAttribute('onselectstart', 'return false;');
+    // For modern browsers
+    element.style.userSelect = 'none';
+}
+
+function toggleTheme() {
+    const bodyElement = document.querySelector('body');
+    bodyElement.classList.toggle('dark-mode'); // Toggle the dark-mode class on the body element
+}
